30112aa26f89e91cd2a56c97ca0ae340c59bd1b4
Sadrican <sadrican.yilmaz@gmail.com>
2023-08-04 00:39:07 +0300

    routes+ controller

---
+++ controllers/contactController.js
@@ -0,0 +1,28 @@
+
+
+
+const getContacts = (req,res)=>{
+    res.status(200).json({message: "get all contacts"})
+}
+const createContact = (req,res)=>{
+    console.log("request body is:",req.body)
+    
+    res.status(201).json({message: `create contact`})
+}
+
+const getContact = (req,res)=>{
+    res.status(200).json({message: `Get contact for ${req.params.id}`})
+}
+
+const updateContact =(req,res)=>{
+    res.status(200).json({message: `Update contact for ${req.params.id}`})
+}
+
+const deleteContact = (req,res)=>{
+    res.status(200).json({message: `Delete contact for ${req.params.id}`})
+}
+
+
+
+ 
+module.exports = {getContacts,createContact, getContact,updateContact,deleteContact};
